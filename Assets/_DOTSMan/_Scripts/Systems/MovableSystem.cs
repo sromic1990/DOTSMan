738265db9ef981fbbac7a86a840fca295a94375f
Sourav Chatterjee <sromic@gmail.com>
2020-09-18 02:01:37 +0530

    Added a Movable Entity to a cube and a player Entity. A Movable System controls the movement of a Movable Entity. A Player System Controls the input given to the Player Entity.

---
+++ Assets/_DOTSMan/_Scripts/Systems/MovableSystem.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class MovableSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        float deltaTime = Time.DeltaTime;
+        Entities.ForEach((ref Movable mov, ref Translation translation, ref Rotation rotation) =>
+        {
+            translation.Value += mov.speed * mov.direction * deltaTime;
+            rotation.Value = math.mul(rotation.Value.value, quaternion.RotateY(mov.speed * deltaTime));
+        }).Schedule();
+    }
+}
---
+++ Assets/_DOTSMan/_Scripts/Systems/PlayerSystem.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PlayerSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+
+        Entities
+            .WithAll<Player>()
+            .ForEach((ref Movable mov) =>
+        {
+            mov.direction = new float3(x, 0, y);
+        }).Schedule();
+    }
+}
